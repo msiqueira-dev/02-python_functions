b85edef823d2beb1f022217d4f450786d4ac0fe8
msiquera-dev <marcusviniciusb.siqueira@hotmail.com>
2024-02-13 14:51:05 -0300

    PT: Adicionando comentário de função EN: Adding comment to function

--- functions.py
+++ functions.py
@@ -6,10 +6,30 @@
 #   This program has the objective of showing how functions work in Python using the temperature conversion as an example
 
 def celsius_to_kelvin(celsius):
+    """ 
+        PT-BR:
+            Isso é um comentário padrão de funções e será ignorado pelo Python
+            Essa função recebe um valor por parametro em celsius, aplica o calculo de conversão e
+            armazena o resultado em uma variavel kelvin, retorna o valor de kelvin
+        EN: 
+            This is a function comment and will be ignored by Python.
+            This function receives a value in the parameter celsius, it them applies the conversion math and
+            stores the result in the variable kelvin, returning it
+    """
     kelvin = celsius + 273
     return kelvin
 
 def celsius_to_fahrenheit(celsius):
+    """ 
+        PT-BR:
+            Isso é um comentário padrão de funções e será ignorado pelo Python
+            Essa função recebe um valor por parametro em celsius, aplica o calculo de conversão e
+            armazena o resultado em uma variavel fahrenheit, retorna o valor de fahrenheit
+        EN: 
+            This is a function comment and will be ignored by Python.
+            This function receives a value in the parameter celsius, it them applies the conversion math and
+            stores the result in the variable fahrenheit, returning it
+    """
     fahrenheit = (celsius * 1.8) + 32
     return fahrenheit
 
